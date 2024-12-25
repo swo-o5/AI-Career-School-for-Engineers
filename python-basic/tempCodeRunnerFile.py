c128a138ae57af94bc53b33593cb3681c196794e
Sungwoo Kim <happy200527@naver.com>
2024-12-25 22:46:02 +0900

    Last commit in python-basic folder

---
+++ python-basic/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+# 변수 words에 비밀 암호가 포함된 문자열이 저장되어 있습니다.
+words = "aucksifkapythoncixkdlovezkigvxi"
+
+# 첫 번째 암호는 words의 마지막 인덱스에 저장되어 있고, 변수 first에 저장하세요.
+first = words[-1]
+
+# 두 번째 암호는 words의 20번째 인덱스부터 23번째 인덱스에 저장되어 있고, 변수 second에 저장하세요.
+second = words[20:24]
+
+# 세 번째 암호는 words의 9번째 인덱스부터 14번째 인덱스에 저장되어 있고, 변수 third에 저장하세요.
+third = words[9:15]
+
+# 최종 암호를 출력하세요.
+print(first, second, third)
